5e53b0cf97c8c3d36575bb014687e30a7cc34bf1
picania <mangustspam@gmail.com>
2018-03-20 15:54:00 +0700

    Вынес макросы для поиска программ на хосте в отдельный скрипт.

---
+++ cmake/find_host.cmake
@@ -0,0 +1,47 @@
+# macro to find packages on the host OS
+macro(find_host_package)
+	set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
+	set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY NEVER)
+	set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE NEVER)
+	
+	if(CMAKE_HOST_WIN32)
+		set(WIN32 1)
+		set(UNIX)
+	elseif(CMAKE_HOST_APPLE)
+		set(APPLE 1)
+		set(UNIX)
+	endif()
+
+	find_package(${ARGN})
+	
+	set(WIN32)
+	set(APPLE)
+	set(UNIX 1)
+	set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM ONLY)
+	set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
+	set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
+endmacro()
+
+# macro to find programs on the host OS
+macro(find_host_program)
+	set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
+	set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY NEVER)
+	set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE NEVER)
+
+	if(CMAKE_HOST_WIN32)
+		set(WIN32 1)
+		set(UNIX)
+	elseif(CMAKE_HOST_APPLE)
+		set(APPLE 1)
+		set(UNIX)
+	endif()
+	
+	find_program(${ARGN})
+	
+	set(WIN32)
+	set(APPLE)
+	set(UNIX 1)
+	set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM ONLY)
+	set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
+	set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
+endmacro()
